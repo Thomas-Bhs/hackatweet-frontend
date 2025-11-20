f65277be44b077d6a42334ddd70bb4c555bb81fc
Thomas <bourchisthomas@gmail.com>
2025-11-20 17:44:57 +0100

    login page and component

---
+++ components/Login.js
@@ -0,0 +1,23 @@
+import styles from '../styles/Home.module.css';
+
+function Login() {
+  return (
+    <div className={styles.container}>
+      
+      
+      <div className={styles.leftside}>
+        <img className={styles.image}/>
+      </div>
+
+    <div className={styles.rightside}>
+        <div className={styles.content}>
+
+        </div>
+    </div>
+
+
+    </div>
+  );
+}
+
+export default Login;
