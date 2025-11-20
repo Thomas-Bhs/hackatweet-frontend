37504293e7da7a4a74b896ee27dfceedcae6897f
Thomas <bourchisthomas@gmail.com>
2025-11-20 17:55:31 +0100

    Update login

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 
+
 //import redux
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import { configureStore } from '@reduxjs/toolkit';
 
+
 const store = configureStore({
   reducer: {},
 });
+
 
 function App({ Component, pageProps }) {
   return (
