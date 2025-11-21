a9d99443a19117611650f3ed3a2ba2812c3fd990
Thomas <bourchisthomas@gmail.com>
2025-11-21 12:58:23 +0100

    signup component

---
+++ components/SignUp.js
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../reducers/user';
+
+
+function SignUp() {
+
+  const dispatch = useDispatch();
+  const [signUpFirstname, setSignUpFirstname] = useState('');
+  const [signUpUsername, setSignUpUsername] = useState('');
+  const [signUpPassword, setSignUpPassword] = useState('');
+
+  const handleRegister = () => {
+    console.log('CLICK REGISTER ✅');
+
+		fetch('http://localhost:3000/users/signup', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+      firstname: signUpFirstname,
+      username: signUpUsername,
+      password: signUpPassword }),
+		}).then(response => response.json())
+			.then(data => {
+          console.log('Backend response:', data);
+				if (data.result) {
+					dispatch(login({ username: signUpUsername, token: data.token }));
+          setSignUpFirstname('');
+					setSignUpUsername('');
+					setSignUpPassword('');
+				}
+			});
+	};
+
+  const userRedux = useSelector(state => state.user.value);
+console.log("USER IN REDUX:", userRedux);
+
+  return (
+    <div className="modal-signup">
+      <div className="modal-content">
+        <p>ModalSignUp</p>
+        <input
+        type="text"
+        placeholder="firsname"
+        name="firstname"
+        value={signUpFirstname}
+        onChange={(e) => setSignUpFirstname(e.target.value)}
+        />
+        <input
+        type="text"
+        placeholder='Username'
+        name="username"
+        value={signUpUsername}
+        onChange={(e) => setSignUpUsername(e.target.value)}
+        />
+        <input
+        type="password"
+        placeholder='Password'
+        name="password"
+        value={signUpPassword}
+        onChange={(e) => setSignUpPassword(e.target.value)}
+        />
+        <button type="button" onClick={handleRegister}>Register</button>
+      </div>
+    </div>
+  );
+
+}
+
+export default SignUp;
--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,26 @@
 import '../styles/globals.css';
 import Head from 'next/head';
-import Login from '../components/Login';
 
 
 //import redux
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import user from '../reducers/user';
 
-// const store = configureStore({
-//   reducer: {},
-// });
+const store = configureStore({
+   reducer: { user },
+     });
 
 
 function App({ Component, pageProps }) {
   return (
-    <div>
-    {/* // <Provider store={store}> */}
+
+    <Provider store={store}>
       <Head>
         <title>Mon app hackaton</title>
       </Head>
-    //   <Component {...pageProps} />
-    {/* // </Provider> */}
-    </div>
+      <Component {...pageProps} />
+    </Provider>
   );
 }
 
