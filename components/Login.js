d0e8de7e58f5a183d151602e7a0ce9fbaf33e10c
Thomas <bourchisthomas@gmail.com>
2025-11-21 14:29:15 +0100

    commit merge signup update

--- components/Login.js
+++ components/Login.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import styles from '../styles/Login.module.css';
+import SignUp from './SignUp';
+
 
 
 function Login() {
+
+const [showSignUp, setShowSignUp] = useState(false)
+
+
   return (
     <div className={styles.container}>
       
@@ -20,13 +27,22 @@ function Login() {
           <h1 className={styles.title}>See what’s happening</h1>
           <h3 className={styles.subtitle}>Join Hackatweet today.</h3>
 
-          <button className={styles.signup}>Sign up</button>
+          <button onClick={()=> setShowSignUp(true)} className={styles.signup}>Sign up</button>
 
           <p className={styles.account}>Already have an account ?</p>
 
           <button className={styles.signup}>Sign in</button>
         </div>
     </div>
+
+      {/* Pop-up Signup */}
+        {showSignUp && (
+          <div className={styles.modalOverlay}>
+            <div className={styles.modalContent}>
+              <SignUp/>
+            </div>
+          </div>
+        )}
 
 
     </div>
---
+++ reducers/user.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: { username: null, token: null },
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.value.username = action.payload.username;
+      state.value.token = action.payload.token;
+    },
+    logout: (state) => {
+      state.value.username = null;
+      state.value.token = null;
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+export default userSlice.reducer;
